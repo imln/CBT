3ad3b6dce43f1170e31c54222cccaf75943f3666
LNGupta <campustray@gmail.com>
2020-04-30 02:32:04 +0530

    commit just after adding database details in setting.py

--- openCBT/settings.py
+++ openCBT/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'openCBT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'DBopenCBT',  
+        'USER':'root',  
+        'PASSWORD':'LNGupta@277501',  
+        'HOST':'localhost',  
+        'PORT':'3306' 
     }
 }
 
