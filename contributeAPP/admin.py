9968ecf1977187461f87e734ef031ed187a779f9
LNGupta <campustray@gmail.com>
2020-04-30 19:34:06 +0530

    commit after adding Exam , ExamBranch , BranchSubject , SubjectTopic models

--- contributeAPP/admin.py
+++ contributeAPP/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from contributeAPP.models import Exam
-
-
+from contributeAPP.models import ExamBranch
+from contributeAPP.models import BranchSubject
+from contributeAPP.models import SubjectTopic
 
 # Exam models register
 class ExamAdmin(admin.ModelAdmin):
@@ -13,3 +14,25 @@ class ExamAdmin(admin.ModelAdmin):
 admin.site.register(Exam,ExamAdmin)
 
 
+# ExamBranch models register
+class ExamBranchAdmin(admin.ModelAdmin):
+	list_display = ['id','name','description','exam_id']
+	ordering = ['id']
+
+admin.site.register(ExamBranch,ExamBranchAdmin)
+
+
+# BranchSubject models register
+class BranchSubjectAdmin(admin.ModelAdmin):
+	list_display = ['id','name']
+	ordering = ['id']
+
+admin.site.register(BranchSubject,BranchSubjectAdmin)
+
+
+# SubjectTopic models register
+class SubjectTopicAdmin(admin.ModelAdmin):
+	list_display = ['id','name','subject_id']
+	ordering = ['id']
+
+admin.site.register(SubjectTopic,SubjectTopicAdmin)
--- contributeAPP/models.py
+++ contributeAPP/models.py
@@ -11,4 +11,38 @@ class Exam(models.Model):
         return self.name
 
 
-# model for adding exam
+# model for adding exam's branch
+class ExamBranch(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=300)
+	exam_id = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+
+# model for adding branch's subject
+class BranchSubject(models.Model):
+	name = models.CharField(max_length=100)
+	ExamBranch = models.ManyToManyField(ExamBranch)
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+	
+# model for adding subject's topic
+class SubjectTopic(models.Model):
+	name = models.CharField(max_length=100)
+	subject_id = models.ForeignKey(BranchSubject, on_delete=models.CASCADE)
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
