9d7221ed98b3e3426b27074c5e4e2c192a09117d
LNGupta <campustray@gmail.com>
2020-04-30 13:08:21 +0530

    commit just after adding Exam model in contributeAPP and also register it in admin.py

--- contributeAPP/admin.py
+++ contributeAPP/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from contributeAPP.models import Exam
+
+
+
+# Exam models register
+class ExamAdmin(admin.ModelAdmin):
+	list_display = ['id','name','description']
+	ordering = ['id']
+
+admin.site.register(Exam,ExamAdmin)
+
+
--- contributeAPP/models.py
+++ contributeAPP/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# model for adding exam
+class Exam(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
+
+
+# model for adding exam
